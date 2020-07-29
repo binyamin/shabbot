cc0dbcfe1c578b0e7ae427809009a824e8827630
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-07-29 00:17:32 -0400

    only fetch time if user is pinged

--- index.js
+++ index.js
@@ -12,21 +12,19 @@ const BUFFER_BEFORE = 180;
 
 module.exports = app => {
   app.on('issue_comment.created', async context => {
-    let resp = await (await fetch("https://www.hebcal.com/shabbat?cfg=json&zip=21208&m=50")).json();
-    let shabbosTimeIn = moment(resp.items.filter(p => p.category==="candles")[0].date).tz("America/New_York").subtract(BUFFER_BEFORE, 'minutes');
-    let shabbosTimeOut = moment(resp.items.filter(p => p.category==="havdalah")[0].date).tz("America/New_York");
-
-    const mtime = moment(shabbosTimeOut).tz("America/New_York").format("h:mma z");
-    
     const {comment} = context.payload;
 
-    if(moment(comment.created_at).tz("America/New_York").isBetween(shabbosTimeIn, shabbosTimeOut)) {
-      if(comment.body.includes("@"+USER_LOGIN) && comment.user.login !== "shabbot[bot]") {
+    if(comment.body.includes("@"+USER_LOGIN) && comment.user.login !== "shabbot[bot]") {
+      let resp = await (await fetch("https://www.hebcal.com/shabbat?cfg=json&zip=21208&m=50")).json();
+      let shabbosTimeIn = moment(resp.items.filter(p => p.category==="candles")[0].date).tz("America/New_York").subtract(BUFFER_BEFORE, 'minutes');
+      let shabbosTimeOut = moment(resp.items.filter(p => p.category==="havdalah")[0].date).tz("America/New_York");      
+
+      if(moment(comment.created_at).tz("America/New_York").isBetween(shabbosTimeIn, shabbosTimeOut)) {
         const issueComment = context.issue({
-          body: COMMENT_BODY.replace("<time>", mtime)
+          body: COMMENT_BODY.replace("<time>", moment(shabbosTimeOut).tz("America/New_York").format("h:mma z"))
         })
         return context.github.issues.createComment(issueComment)
-      }      
+      }
     }
   })
 }

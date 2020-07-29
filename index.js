987433743753a00fa02393869f38d2804f9ecb39
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-07-28 23:41:29 -0400

    add initial working code

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+const fetch = require("node-fetch");
+const moment = require("moment-timezone");
+
+const USER_LOGIN = "b3u";
+const COMMENT_BODY = `@${USER_LOGIN} is not available until <time> on Saturday evening.`;
+const TEST = true
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
+
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.on('issue_comment.created', async context => {
+    let resp = await (await fetch("https://www.hebcal.com/shabbat?cfg=json&zip=21208&m=50")).json();
+    let shabbosTimeIn = resp.items.filter(p => p.category==="candles")[0].date;
+    let shabbosTimeOut = resp.items.filter(p => p.category==="havdalah")[0].date;
+    
+    const mtime = moment(shabbosTimeOut).tz("America/New_York").format("h:mma z");
+    
+    const {comment} = context.payload;
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
+    if(moment(comment.created_at).isBetween(shabbosTimeIn, shabbosTimeOut) || TEST) {
+      if(comment.body.includes("@"+USER_LOGIN) && comment.user.login !== "shabbot[bot]") {
+        const issueComment = context.issue({
+          body: COMMENT_BODY.replace("<time>", mtime)
+        })
+        return context.github.issues.createComment(issueComment)
+      }      
+    }
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }

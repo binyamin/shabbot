4cfc47a360fac112440a14563bf56b14dd73f769
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-07-29 00:00:17 -0400

    add buffer time before

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const moment = require("moment-timezone");
 
 const USER_LOGIN = "b3u";
 const COMMENT_BODY = `@${USER_LOGIN} is not available until <time> on Saturday evening.`;
-const TEST = true
+const BUFFER_BEFORE = 180;
 
 /**
  * This is the main entrypoint to your Probot app
@@ -13,14 +13,14 @@ const TEST = true
 module.exports = app => {
   app.on('issue_comment.created', async context => {
     let resp = await (await fetch("https://www.hebcal.com/shabbat?cfg=json&zip=21208&m=50")).json();
-    let shabbosTimeIn = resp.items.filter(p => p.category==="candles")[0].date;
-    let shabbosTimeOut = resp.items.filter(p => p.category==="havdalah")[0].date;
-    
+    let shabbosTimeIn = moment(resp.items.filter(p => p.category==="candles")[0].date).tz("America/New_York").subtract(BUFFER_BEFORE, 'minutes');
+    let shabbosTimeOut = moment(resp.items.filter(p => p.category==="havdalah")[0].date).tz("America/New_York");
+
     const mtime = moment(shabbosTimeOut).tz("America/New_York").format("h:mma z");
     
     const {comment} = context.payload;
 
-    if(moment(comment.created_at).isBetween(shabbosTimeIn, shabbosTimeOut) || TEST) {
+    if(moment(comment.created_at).tz("America/New_York").isBetween(shabbosTimeIn, shabbosTimeOut)) {
       if(comment.body.includes("@"+USER_LOGIN) && comment.user.login !== "shabbot[bot]") {
         const issueComment = context.issue({
           body: COMMENT_BODY.replace("<time>", mtime)

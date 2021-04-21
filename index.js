983b5c296832e53a147de21d52d16f21b923b756
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-04-21 16:37:12 +0000

    linting commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const fetch = require("node-fetch");
-const moment = require("moment-timezone");
+const fetch = require('node-fetch')
+const moment = require('moment-timezone')
 
-const USER_LOGIN = "binyamin";
-const COMMENT_BODY = `@${USER_LOGIN} is not available until <time> on Saturday evening.`;
-const BUFFER_BEFORE = 180;
+const USER_LOGIN = 'binyamin'
+const COMMENT_BODY = `@${USER_LOGIN} is not available until <time> on Saturday evening.`
+const BUFFER_BEFORE = 180
 
 /**
  * This is the main entrypoint to your Probot app
@@ -12,16 +12,16 @@ const BUFFER_BEFORE = 180;
 
 module.exports = app => {
   app.on('issue_comment.created', async context => {
-    const {comment} = context.payload;
+    const { comment } = context.payload
 
-    if(comment.body.includes("@"+USER_LOGIN) && comment.user.login !== "shabbot[bot]") {
-      let resp = await (await fetch("https://www.hebcal.com/shabbat?cfg=json&zip=21208&m=50")).json();
-      let shabbosTimeIn = moment(resp.items.filter(p => p.category==="candles")[0].date).tz("America/New_York").subtract(BUFFER_BEFORE, 'minutes');
-      let shabbosTimeOut = moment(resp.items.filter(p => p.category==="havdalah")[0].date).tz("America/New_York");      
+    if (comment.body.includes('@' + USER_LOGIN) && comment.user.login !== 'shabbot[bot]') {
+      const resp = await (await fetch('https://www.hebcal.com/shabbat?cfg=json&zip=21208&m=50')).json()
+      const shabbosTimeIn = moment(resp.items.filter(p => p.category === 'candles')[0].date).tz('America/New_York').subtract(BUFFER_BEFORE, 'minutes')
+      const shabbosTimeOut = moment(resp.items.filter(p => p.category === 'havdalah')[0].date).tz('America/New_York')
 
-      if(moment(comment.created_at).tz("America/New_York").isBetween(shabbosTimeIn, shabbosTimeOut)) {
+      if (moment(comment.created_at).tz('America/New_York').isBetween(shabbosTimeIn, shabbosTimeOut)) {
         const issueComment = context.issue({
-          body: COMMENT_BODY.replace("<time>", moment(shabbosTimeOut).tz("America/New_York").format("h:mma z"))
+          body: COMMENT_BODY.replace('<time>', moment(shabbosTimeOut).tz('America/New_York').format('h:mma z'))
         })
         return context.github.issues.createComment(issueComment)
       }
